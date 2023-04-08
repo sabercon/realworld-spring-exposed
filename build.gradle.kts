77f1df53b1353fb57cbcbdceec4e68dcc7236141
SaberCon <894876785@qq.com>
2023-04-08 11:37:56 +0800

    Add other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,15 @@ repositories {
 
 dependencies {
     implementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
+    implementation(platform("io.kotest:kotest-bom:5.5.5"))
 
     implementation("org.springframework.boot:spring-boot-starter-jooq")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.flywaydb:flyway-core")
+    // https://mvnrepository.com/artifact/com.auth0/java-jwt
+    implementation("com.auth0:java-jwt:4.4.0")
+    // https://mvnrepository.com/artifact/com.github.slugify/slugify
+    implementation("com.github.slugify:slugify:3.0.2")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -33,6 +38,10 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
+    testImplementation("io.kotest:kotest-runner-junit5")
+    testImplementation("io.kotest:kotest-assertions-core")
+    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
 
 tasks.withType<KotlinCompile> {
