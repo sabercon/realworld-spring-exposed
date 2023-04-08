94841a65da7ddaa1868052d29e179a9b35763271
SaberCon <894876785@qq.com>
2023-04-08 10:24:35 +0800

    Init from Spring Initializr

---
+++ src/main/kotlin/cn/sabercon/realworld/RealworldApplication.kt
@@ -0,0 +1,11 @@
+package cn.sabercon.realworld
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealworldApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealworldApplication>(*args)
+}
---
+++ src/test/kotlin/cn/sabercon/realworld/RealworldApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.sabercon.realworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealworldApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
