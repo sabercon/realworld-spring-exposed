7557a1252799d2c2790e05a6c309f965a27c9c21
SaberCon <894876785@qq.com>
2023-05-21 21:17:06 +0800

    Upgrade SpringBoot and Kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.5"
+    id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.20"
-    kotlin("plugin.spring") version "1.8.20"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
 
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
     id("org.jetbrains.kotlinx.kover") version "0.6.1"
@@ -19,8 +19,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
-    implementation(platform("io.kotest:kotest-bom:5.5.5"))
+    implementation(platform("io.kotest:kotest-bom:5.6.2"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -33,9 +32,12 @@ dependencies {
     implementation("com.auth0:java-jwt:4.4.0")
     implementation("com.github.slugify:slugify:3.0.2")
 
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
     runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("io.kotest:kotest-runner-junit5")
