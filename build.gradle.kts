63dfd3f0ab91f06801892de5c30ef3554b7da582
SaberCon <894876785@qq.com>
2023-05-27 23:35:32 +0800

    Add Detekt Formatting Rule Set

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("io.mockk:mockk:1.13.5")
+
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
 }
 
 tasks.withType<KotlinCompile> {
